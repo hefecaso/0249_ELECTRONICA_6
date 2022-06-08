ebc2126166a29939c72ef47f7d49e7040d075979
hefecaso <hfcarrerasoto@ieee.org>
2022-06-08 02:46:30 -0600

    Error corregido, solicitud grados, chekpoint

--- Servo/Pruebas_de_movimiento.py
+++ Servo/Pruebas_de_movimiento.py
@@ -43,7 +43,7 @@ while True:
     else:
         print("Debe de ingresar un ángulo entre 0° y 180°")
         pwm.start(angle_to_percent(0))
-        pwm.stop()
-        GPIO.cleanup()
+        #pwm.stop()
+        #GPIO.cleanup()
 
 #https://raspberrypi-espana.es/servo-frambuesa-pi/
