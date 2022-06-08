057e98a371b69ebd907ff40bd8a6d46bf5f82a21
hefecaso <hfcarrerasoto@ieee.org>
2022-06-08 02:07:29 -0600

    Pruebaa

--- Servo/Pruebas_de_movimiento.py
+++ Servo/Pruebas_de_movimiento.py
@@ -1,161 +1,89 @@
-#//////////////////////////////////////////////////////////////////////
-#/  INTERPRETE  ///////////////////////////////////////////////////////
-#//////////////////////////////////////////////////////////////////////
-
-#!/usr/bin/python3
-#es una ruta del interprete, en este caso el de python3
-
-#//////////////////////////////////////////////////////////////////////
-#/  LIBRERIAS  ////////////////////////////////////////////////////////
-#//////////////////////////////////////////////////////////////////////
-
-#importar las librerias
-import time
-import pigpio
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#################################################
+#                                               #
+# Servos control module                         #
+# Authors: Ismael Tobar and Raquel Muñoz        #
+#                                               #
+#################################################
+#----Libraries
 import RPi.GPIO as GPIO
-import math
+import threading from time
+import sleep
+# Constants:
+T = 20 #signal period (ms).
+STEPS = 50 #Number of iteration that the signal is repeated. This is necesary to the servo has time to change.
+MIN_PULSE = 0.83 #Min width of pulse. (ms)
+MAX_PULSE = 2.2 #Max width of pulse. (ms) #2.6
+MIN_DEGREE = 0 MAX_DEGREE = 180 DEGREE_MS = (MAX_PULSE-MIN_PULSE)/180 #Transform the degree to applicate into a time (in ms) variable
+INITIAL_PULSE = 1.74
 
-#//////////////////////////////////////////////////////////////////////
-#/  CONFIGURACION  ///////////////////////////////////////////////
-#//////////////////////////////////////////////////////////////////////
-servo = 11
-GPIO.setmode(GPIO.BOARD) #utilizar el sistema de numeracion fisica
-GPIO.setup(servo, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) #configurar el pin 13 como entrada e internamente una resistencia pull down
+#Variables
+servo_stopped = True
+#Servos' pins
+S1 = 17
 
-#//////////////////////////////////////////////////////////////////////
-#/  VARIABLES GLOBALES  ///////////////////////////////////////////////
-#//////////////////////////////////////////////////////////////////////
+# Raspberry Pi GPIO Configuration:
+def setup():
+  GPIO.setmode(GPIO.BCM)
+  GPIO.setup(S1,GPIO.OUT)
 
-pi = pigpio.pi() #inicializar la variable pi
+  GPIO.output(S1, False)
 
-#//////////////////////////////////////////////////////////////////////
-#/  PRINCIPAL  ///////////////////////////////////////////////////////
-#//////////////////////////////////////////////////////////////////////
+  print('Servo: Setup')
 
-pi.set_servo_pulsewidth(servo,2390) #girar el servo hasta 0 grados como inicio
-time.sleep(0.5) #esperar medio segundo
-#input('Pulse la tecla enter para seguir el paso ')
+def thread_servo(d,s): #The pwm signal is done through Software global servo_stopped
+  h = d/1000
+  l = (T-d)/1000
+  if MIN_PULSE <= d <= MAX_PULSE:
+    for i in range (0,STEPS):
+      GPIO.output(s, True)
+      sleep(h)
+      GPIO.output(s, False)
+      sleep(l)
+      servo_stopped = True
+  clean()
+  #Each time you finish moving the servo is necessary to clean the pins so they do not move with noise
 
-try:
-        print('Pulse el boton de interrupcion para comenzar')
-        GPIO.wait_for_edge(servo, GPIO.RISING, timeout=5000) #configurar el pin 13 una interrupcion de tipo espera, con un maximo tiempo de 5 segundos
-        while True:
-                #se espera a que el usuario introduzca un valor de grado
-                grados = float(input('Introducza el valor en grados, entre 0 y 180: '))
-                radianes = ((2 * math.pi) / 360) * grados
-                print("El angulo introducido es", radianes, "radianes")
-                grados = 180 - grados
-                #conversion de grados a radianes y lo imprime
-                #compara los grados y dependiendo de la condicion realiza la funcion utilizando valores de microsegundos empiricamente
-                if grados < 0 or grados >180:
-                    #imprime cuando el usuario introduce un valor fuera del rango
-                    print("El dato introducido es erroneo")
-                    print("Debe de introducir entre 0 y 180 grados")
-                elif grados == 0:
-                    pi.set_servo_pulsewidth(servo,510)
-                elif grados < 10:
-                        Nms = grados*4+515
-                        pi.set_servo_pulsewidth(servo,Nms)
-                elif grados == 10:
-                        pi.set_servo_pulsewidth(servo,555)
-                elif grados < 20:
-                        Nms = (grados-10)*8+555
-                        pi.set_servo_pulsewidth(servo,Nms)
-                elif grados == 20:
-                        pi.set_servo_pulsewidth(servo,635)
-                elif grados < 30:
-                        Nms = (grados-20)*9+635
-                        pi.set_servo_pulsewidth(servo,Nms)
-                elif grados == 30:
-                        pi.set_servo_pulsewidth(servo,725)
-                elif grados < 40:
-                        Nms = (grados-30)*9.5+725
-                        pi.set_servo_pulsewidth(servo,Nms)
-                elif grados == 40:
-                        pi.set_servo_pulsewidth(servo,820)
-                elif grados < 45:
-                        Nms = (grados-40)*1+820
-                        pi.set_servo_pulsewidth(servo,Nms)
-                elif grados == 45:
-                        pi.set_servo_pulsewidth(servo,825)
-                elif grados < 50:
-                        Nms = (grados-45)*25+825
-                        pi.set_servo_pulsewidth(servo,Nms)
-                elif grados == 50:
-                        pi.set_servo_pulsewidth(servo,950)
-                elif grados < 60:
-                        Nms = (grados-40)*10+950
-                        pi.set_servo_pulsewidth(servo,Nms)
-                elif grados == 60:
-                        pi.set_servo_pulsewidth(servo,1050)
-                elif grados < 70:
-                        Nms = (grados-60)*8+1050
-                        pi.set_servo_pulsewidth(servo,Nms)
-                elif grados == 70:
-                        pi.set_servo_pulsewidth(servo,1130)
-                elif grados < 80:
-                        Nms = (grados-70)*12+1130
-                        pi.set_servo_pulsewidth(servo,Nms)
-                elif grados == 80:
-                        pi.set_servo_pulsewidth(servo,1250)
-                elif grados < 90:
-                        Nms = (grados-80)*5+1250
-                        pi.set_servo_pulsewidth(servo,Nms)
-                elif grados == 90:
-                        pi.set_servo_pulsewidth(servo,1300)
-                elif grados < 100:
-                        Nms = (grados-90)*12.5+1300
-                        pi.set_servo_pulsewidth(servo,Nms)
-                elif grados == 100:
-                        pi.set_servo_pulsewidth(servo,1425)
-                elif grados < 110:
-                        Nms = (grados-100)*16.5+1425
-                        pi.set_servo_pulsewidth(servo,Nms)
-                elif grados == 110:
-                        pi.set_servo_pulsewidth(servo,1590)
-                elif grados == 122:
-                        pi.set_servo_pulsewidth(servo,1750)
-                elif grados < 120:
-                        Nms = (grados-110)*11+1590
-                        pi.set_servo_pulsewidth(servo,Nms)
-                elif grados == 120:
-                        pi.set_servo_pulsewidth(servo,1700)
-                elif grados < 130:
-                        Nms = (grados-120)*12.5+1700
-                        pi.set_servo_pulsewidth(servo,Nms)
-                elif grados == 130:
-                        pi.set_servo_pulsewidth(servo,1825)
-                elif grados < 140:
-                        Nms = (grados-130)*11.5+1825
-                        pi.set_servo_pulsewidth(servo,Nms)
-                elif grados == 140:
-                        pi.set_servo_pulsewidth(servo,1940)
-                elif grados < 150:
-                        Nms = (grados-140)*11+1940
-                        pi.set_servo_pulsewidth(servo,Nms)
-                elif grados == 150:
-                        pi.set_servo_pulsewidth(servo,2050)
-                elif grados < 160:
-                        Nms = (grados-150)*12+2050
-                        pi.set_servo_pulsewidth(servo,Nms)
-                elif grados == 160:
-                        pi.set_servo_pulsewidth(servo,2170)
-                elif grados < 170:
-                        Nms = (grados-160)*11+2170
-                        pi.set_servo_pulsewidth(servo,Nms)
-                elif grados == 170:
-                        pi.set_servo_pulsewidth(servo,2280)
-                elif grados < 180:
-                        Nms = (grados-170)*10+2280
-                        pi.set_servo_pulsewidth(servo,Nms)
-                elif grados == 180:
-                        pi.set_servo_pulsewidth(servo,2390)
-                time.sleep(1)
-except KeyboardInterrupt: #si el usuario pulsa CONTROL + C entonces imprime lo siguiente
-    print("")
-    print("Salir de programa")
-except:
-    print("Otros problemas ocurridos")
-finally:
-    pi.stop()
-    GPIO.cleanup() #limpiamos los pines GPIO y paramos el servo en cualquier caso aunque ocurra una excepcion
+  #move the selected servo (n=1 or n=2) to the degree indicated (between 0 and 180)
+def moveServo(degree,n):
+  print('moveServo',degree,n)
+  global servo_stopped
+  try:
+    if servo_stopped:
+      if MIN_DEGREE <= degree <= MAX_DEGREE:
+        setup()
+        print(degree)
+        if n == 1:
+          thread_s = threading.Thread(target=thread_servo, args=((degree * DEGREE_MS) + MIN_PULSE, S1, ))
+          servo_stopped = False
+          thread_s.start()
+
+  except Exception as e:
+    print("Servo: Error starting servo thread")
+    print(e)
+
+def clean():
+  GPIO.cleanup(S1)
+
+
+def stop_servo():
+  global servo_stopped
+  servo_stopped = True
+
+if __name__ == "__main__":
+  terminate = False #finish the while()
+  # if run directly we'll just create an instance of the class and output
+  setup()
+  ser = 3
+  while terminate == False:
+    sleep(0.1)
+    ser = 3
+    ser = input('Enter servo 1 or 2: (0 to exit) : ')
+    if ser == 0:
+      terminate = True
+    else:
+      deg = input('Enter degree (0 to 180) : ') #The degrees are asked
+      if 1<= ser <= 2:
+        moveServo(deg,ser)
+  GPIO.cleanup() print('Servo: Exit servo')
