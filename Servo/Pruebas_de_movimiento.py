e7a44b93192029a823c998cd6cb450cfe8102972
hefecaso <hfcarrerasoto@ieee.org>
2022-06-08 01:59:53 -0600

    8

--- Servo/Pruebas_de_movimiento.py
+++ Servo/Pruebas_de_movimiento.py
@@ -1,10 +1,31 @@
-from machine import Pin, PWM
+
 import utime
+
+
+# Importamos librerías de python
+import RPi.GPIO as GPIO
+
+# Pondemos en sleep los pines por un tiempo determinado
+import time
+
+#Nompramos el númmero de pin físico
+servoPIN = 11
+
+# Con este vamos a definir por enumeración de GPIO
+#GPIO.setmode(GPIO.BCM)
+
+# Con este vamos a definir por enmeración física de pines
+GPIO.setmode(GPIO.BOARD)
+
+# Indicamos si el servo será de salida o entrada de señal
+GPIO.setup(servoPIN, GPIO.OUT)
+
+
 def main():
     #Configura el Servo de 180
-    servo_180 = PWM(Pin(15))
+    servo_180 = PWM(servoPIN)
     servo_180.freq(50)
-    
+
 
     while True:
         angulo = float(input('Ingrese un ángulo: '))
