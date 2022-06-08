0dcb96701b743c5930fd24ea3ad3d6b7d96cc04c
hefecaso <hfcarrerasoto@ieee.org>
2022-06-08 01:45:08 -0600

    Actualizando

--- Servo/Pruebas_de_movimiento.py
+++ Servo/Pruebas_de_movimiento.py
@@ -20,7 +20,7 @@ GPIO.setmode(GPIO.BOARD) #Use Board numerotation mode
 GPIO.setwarnings(False) #Disable warnings
 
 #Use pin 12 for PWM signal
-pwm_gpio = 12
+pwm_gpio = 11
 frequence = 50
 GPIO.setup(pwm_gpio, GPIO.OUT)
 pwm = GPIO.PWM(pwm_gpio, frequence)
