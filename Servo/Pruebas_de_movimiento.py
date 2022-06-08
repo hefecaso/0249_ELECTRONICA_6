5098d51b2f50fb1c203020188ebe888638299eb1
hefecaso <hfcarrerasoto@ieee.org>
2022-06-08 15:42:25 -0600

    Agregando condicionales, checkpoint?

--- Servo/Pruebas_de_movimiento.py
+++ Servo/Pruebas_de_movimiento.py
@@ -1,42 +1,58 @@
-#!/usr/bin/env python3
-#-- coding: utf-8 --
 import RPi.GPIO as GPIO
 import time
 
 
-GPIO.setmode(GPIO.BOARD) #Use Board numerotation mode
-GPIO.setwarnings(False) #Disable warnings
+GPIO.setmode(GPIO.BOARD) # Use Board numerotation mode
+GPIO.setwarnings(False) # Disable warnings
 
-#Use pin 11 for PWM signal
+# Use pin 11 for PWM signal
 servo = 11
 frequence = 50
 GPIO.setup(servo, GPIO.OUT)
 pwm = GPIO.PWM(servo, frequence)
 
 
-#Convirtiendo ángulos a ciclos de trabajo
+#C onvirtiendo ángulos a ciclos de trabajo
 def angulo_giro(angulo):
     if angulo > 180 or angulo < 0 :
         return False
     giro = (angulo)/18 +2
     return giro
 
+# Ingreso del ángulo por el usuario
 def movimiento():
-    angulo = float(input("Ingrese un águlo: "))
     pwm.start(angulo_giro(angulo))
 
+# Iniciando pwm en 0
 pwm.start(0)
 
+# Iniciando loop
 while True:
-    #Init at 0°
-    movimiento()
+    angulo = float(input("Ingrese un águlo: "))
+    salir = 181
+    if 180 >= angulo >= 0:
+        movimiento()
 
+    elif angulo >= 180 and 0 <= angulo:
+        print("Debe de ingresar un ángulo entre 180° y 0°")
 
+    else:
+        #Close GPIO & cleanup
+        pwm.stop()
+        GPIO.cleanup()
+        break
+        
 ''''
-        DC 7% |neut | 90°
-        DC 2% | min | 0°
-        DC 12%| max | 180°
+        DC 7% | neut | 90°
+        DC 2% | min  | 0°
+        DC 12%| max  | 180°
 '''
+#################
+#   Referencias #
+#################
+
 # Tutorial: https://raspberrypi-espana.es/servo-frambuesa-pi/
+
+# Referencias importantes
 # Teória y cálculos: https://www.learnrobotics.org/blog/raspberry-pi-servo-motor/?utm_source=youtube&utm_medium=description&utm_campaign=link_in_description_FYb7Pr2XNxE#Step-3-Calculate-duty-cycle-to-degrees-formula-for-the-Servo-Motor
 # Teoría youtube: https://www.youtube.com/watch?v=ddlDgUymbxc&ab_channel=GavenMacDonald
