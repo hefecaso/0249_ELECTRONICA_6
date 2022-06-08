f1416a2e39d7fdff26eebf952d8c5f18a20bd3e5
hefecaso <hfcarrerasoto@ieee.org>
2022-06-08 15:29:41 -0600

    Optimizando, checkpoint anterior commit

--- Servo/Pruebas_de_movimiento.py
+++ Servo/Pruebas_de_movimiento.py
@@ -4,20 +4,6 @@ import RPi.GPIO as GPIO
 import time
 
 
-#Set function to calculate percent from angle
-def angle_to_percent (angle) :
-    if angle > 180 or angle < 0 :
-        return False
-
-    start = 4
-    end = 12.5
-    ratio = (end - start)/180 #Calcul ratio from angle to percent
-
-    angle_as_percent = angle * ratio
-
-    return start + angle_as_percent
-
-
 GPIO.setmode(GPIO.BOARD) #Use Board numerotation mode
 GPIO.setwarnings(False) #Disable warnings
 
@@ -27,24 +13,30 @@ frequence = 50
 GPIO.setup(servo, GPIO.OUT)
 pwm = GPIO.PWM(servo, frequence)
 
+
+#Convirtiendo ángulos a ciclos de trabajo
 def angulo_giro(angulo):
     if angulo > 180 or angulo < 0 :
-        return False    
+        return False
     giro = (angulo)/18 +2
     return giro
 
-#pwm.start(7.5)
-while True:
-    #Init at 0°
-
-
+def movimiento():
     angulo = float(input("Ingrese un águlo: "))
     pwm.start(angulo_giro(angulo))
 
+pwm.start(0)
+
+while True:
+    #Init at 0°
+    movimiento()
+
 
 ''''
-        7 = 90
-        2 = 0
-        12 = 180
+        DC 7% |neut | 90°
+        DC 2% | min | 0°
+        DC 12%| max | 180°
 '''
-#https://raspberrypi-espana.es/servo-frambuesa-pi/
+# Tutorial: https://raspberrypi-espana.es/servo-frambuesa-pi/
+# Teória y cálculos: https://www.learnrobotics.org/blog/raspberry-pi-servo-motor/?utm_source=youtube&utm_medium=description&utm_campaign=link_in_description_FYb7Pr2XNxE#Step-3-Calculate-duty-cycle-to-degrees-formula-for-the-Servo-Motor
+# Teoría youtube: https://www.youtube.com/watch?v=ddlDgUymbxc&ab_channel=GavenMacDonald
