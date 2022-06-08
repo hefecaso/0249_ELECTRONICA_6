5e9cc0eab20a13497397d6ea7d37fad42d258dcc
hefecaso <hfcarrerasoto@ieee.org>
2022-06-08 00:55:48 -0600

    Modificando código

--- Servo/Puebas_de_movimiento.py
+++ Servo/Puebas_de_movimiento.py
@@ -1,30 +1,10 @@
 import RPi.GPIO as GPIO
-import time
 
-servoPIN = 11
+servo = 111
+
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(servoPIN, GPIO.OUT)
+GPIO.setmode(GPIO.OUT)
 
-p = GPIO.PWM(servoPIN, 50) # GPIO 17 for PWM with 50Hz
-p.start(2.5)
-try:
-  while True:
-    p.ChangeDutyCycle(5)
-    time.sleep(0.5)
-    p.ChangeDutyCycle(7.5)
-    time.sleep(0.5)
-    p.ChangeDutyCycle(10)
-    time.sleep(0.5)
-    p.ChangeDutyCycle(12.5)
-    time.sleep(0.5)
-    p.ChangeDutyCycle(10)
-    time.sleep(0.5)
-    p.ChangeDutyCycle(7.5)
-    time.sleep(0.5)
-    p.ChangeDutyCycle(5)
-    time.sleep(0.5)
-    p.ChangeDutyCycle(2.5)
-    time.sleep(0.5)
-except KeyboardInterrupt:
-  p.stop()
-  GPIO.cleanup()
+pwm = GPIO.PWM(servo, 50)
+pwm.start(5)
+pwm.ChangeDutyCucle(2)
