4548a7bba2dcb5db8c9478a02b449a8da78e58c6
hefecaso <hfcarrerasoto@ieee.org>
2022-06-09 16:25:07 -0600

    Modificando con funciones

--- Stepper/prueba_stepper.py
+++ Stepper/prueba_stepper.py
@@ -18,164 +18,106 @@ GPIO.setup(out1,GPIO.OUT)
 GPIO.setup(out2,GPIO.OUT)
 GPIO.setup(out3,GPIO.OUT)
 GPIO.setup(out4,GPIO.OUT)
+#print("Ingrese el valor de azimut (0-360°)")
+#deg = int(input())
+#def deg-re
 
-print ()"First calibrate by giving some +ve and -ve values.....")
+def giro_stepper(x):
+    if i==0:
+     GPIO.output(out1,GPIO.HIGH)
+     GPIO.output(out2,GPIO.LOW)
+     GPIO.output(out3,GPIO.LOW)
+     GPIO.output(out4,GPIO.LOW)
+     time.sleep(0.003)
+     #time.sleep(1)
+    elif i==1:
+     GPIO.output(out1,GPIO.HIGH)
+     GPIO.output(out2,GPIO.HIGH)
+     GPIO.output(out3,GPIO.LOW)
+     GPIO.output(out4,GPIO.LOW)
+     time.sleep(0.003)
+     #time.sleep(1)
+    elif i==2:
+     GPIO.output(out1,GPIO.LOW)
+     GPIO.output(out2,GPIO.HIGH)
+     GPIO.output(out3,GPIO.LOW)
+     GPIO.output(out4,GPIO.LOW)
+     time.sleep(0.003)
+     #time.sleep(1)
+    elif i==3:
+     GPIO.output(out1,GPIO.LOW)
+     GPIO.output(out2,GPIO.HIGH)
+     GPIO.output(out3,GPIO.HIGH)
+     GPIO.output(out4,GPIO.LOW)
+     time.sleep(0.003)
+     #time.sleep(1)
+    elif i==4:
+     GPIO.output(out1,GPIO.LOW)
+     GPIO.output(out2,GPIO.LOW)
+     GPIO.output(out3,GPIO.HIGH)
+     GPIO.output(out4,GPIO.LOW)
+     time.sleep(0.003)
+     #time.sleep(1)
+    elif i==5:
+     GPIO.output(out1,GPIO.LOW)
+     GPIO.output(out2,GPIO.LOW)
+     GPIO.output(out3,GPIO.HIGH)
+     GPIO.output(out4,GPIO.HIGH)
+     time.sleep(0.003)
+     #time.sleep(1)
+    elif i==6:
+     GPIO.output(out1,GPIO.LOW)
+     GPIO.output(out2,GPIO.LOW)
+     GPIO.output(out3,GPIO.LOW)
+     GPIO.output(out4,GPIO.HIGH)
+     time.sleep(0.003)
+     #time.sleep(1)
+    elif i==7:
+     GPIO.output(out1,GPIO.HIGH)
+     GPIO.output(out2,GPIO.LOW)
+     GPIO.output(out3,GPIO.LOW)
+     GPIO.output(out4,GPIO.HIGH)
+     time.sleep(0.003)
 
+
+
+
+# Iniciando loop
 
 try:
-   while(1):
-      GPIO.output(out1,GPIO.LOW)
-      GPIO.output(out2,GPIO.LOW)
-      GPIO.output(out3,GPIO.LOW)
-      GPIO.output(out4,GPIO.LOW)
-      Print(ingrese un valor entre 0 y 4096 para rotar un angulo de 0 a 360)
-      x = int(input())
-      if x>0 and x<=4096:
-          for y in range(x,0,-1):
-              if negative==1:
-                  if i==7:
-                      i=0
-                  else:
-                      i=i+1
-                  y=y+2
-                  negative=0
-              positive=1
-              #print((x+1)-y)
-              if i==0:
-                  GPIO.output(out1,GPIO.HIGH)
-                  GPIO.output(out2,GPIO.LOW)
-                  GPIO.output(out3,GPIO.LOW)
-                  GPIO.output(out4,GPIO.LOW)
-                  time.sleep(0.003)
-                  #time.sleep(1)
-              elif i==1:
-                  GPIO.output(out1,GPIO.HIGH)
-                  GPIO.output(out2,GPIO.HIGH)
-                  GPIO.output(out3,GPIO.LOW)
-                  GPIO.output(out4,GPIO.LOW)
-                  time.sleep(0.003)
-                  #time.sleep(1)
-              elif i==2:
-                  GPIO.output(out1,GPIO.LOW)
-                  GPIO.output(out2,GPIO.HIGH)
-                  GPIO.output(out3,GPIO.LOW)
-                  GPIO.output(out4,GPIO.LOW)
-                  time.sleep(0.003)
-                  #time.sleep(1)
-              elif i==3:
-                  GPIO.output(out1,GPIO.LOW)
-                  GPIO.output(out2,GPIO.HIGH)
-                  GPIO.output(out3,GPIO.HIGH)
-                  GPIO.output(out4,GPIO.LOW)
-                  time.sleep(0.003)
-                  #time.sleep(1)
-              elif i==4:
-                  GPIO.output(out1,GPIO.LOW)
-                  GPIO.output(out2,GPIO.LOW)
-                  GPIO.output(out3,GPIO.HIGH)
-                  GPIO.output(out4,GPIO.LOW)
-                  time.sleep(0.003)
-                  #time.sleep(1)
-              elif i==5:
-                  GPIO.output(out1,GPIO.LOW)
-                  GPIO.output(out2,GPIO.LOW)
-                  GPIO.output(out3,GPIO.HIGH)
-                  GPIO.output(out4,GPIO.HIGH)
-                  time.sleep(0.003)
-                  #time.sleep(1)
-              elif i==6:
-                  GPIO.output(out1,GPIO.LOW)
-                  GPIO.output(out2,GPIO.LOW)
-                  GPIO.output(out3,GPIO.LOW)
-                  GPIO.output(out4,GPIO.HIGH)
-                  time.sleep(0.003)
-                  #time.sleep(1)
-              elif i==7:
-                  GPIO.output(out1,GPIO.HIGH)
-                  GPIO.output(out2,GPIO.LOW)
-                  GPIO.output(out3,GPIO.LOW)
-                  GPIO.output(out4,GPIO.HIGH)
-                  time.sleep(0.003)
-                  #time.sleep(1)
-              if i==7:
-                  i=0
-                  continue
-              i=i+1
+    while(1):
 
+    GPIO.output(out1,GPIO.LOW)
+    GPIO.output(out2,GPIO.LOW)
+    GPIO.output(out3,GPIO.LOW)
+    GPIO.output(out4,GPIO.LOW)
+    deg = int(input("\ningrese un valor para rotar un angulo de 0 a 360"))
+    x = int(-1*(deg*4096)/(360))
 
-      elif x<0 and x>=-4096:
-          x=x*-1
-          for y in range(x,0,-1):
-              if positive==1:
-                  if i==0:
-                      i=7
-                  else:
-                      i=i-1
-                  y=y+3
-                  positive=0
-              negative=1
-              #print((x+1)-y)
-              if i==0:
-                  GPIO.output(out1,GPIO.HIGH)
-                  GPIO.output(out2,GPIO.LOW)
-                  GPIO.output(out3,GPIO.LOW)
-                  GPIO.output(out4,GPIO.LOW)
-                  time.sleep(0.003)
-                  #time.sleep(1)
-              elif i==1:
-                  GPIO.output(out1,GPIO.HIGH)
-                  GPIO.output(out2,GPIO.HIGH)
-                  GPIO.output(out3,GPIO.LOW)
-                  GPIO.output(out4,GPIO.LOW)
-                  time.sleep(0.003)
-                  #time.sleep(1)
-              elif i==2:
-                  GPIO.output(out1,GPIO.LOW)
-                  GPIO.output(out2,GPIO.HIGH)
-                  GPIO.output(out3,GPIO.LOW)
-                  GPIO.output(out4,GPIO.LOW)
-                  time.sleep(0.003)
-                  #time.sleep(1)
-              elif i==3:
-                  GPIO.output(out1,GPIO.LOW)
-                  GPIO.output(out2,GPIO.HIGH)
-                  GPIO.output(out3,GPIO.HIGH)
-                  GPIO.output(out4,GPIO.LOW)
-                  time.sleep(0.003)
-                  #time.sleep(1)
-              elif i==4:
-                  GPIO.output(out1,GPIO.LOW)
-                  GPIO.output(out2,GPIO.LOW)
-                  GPIO.output(out3,GPIO.HIGH)
-                  GPIO.output(out4,GPIO.LOW)
-                  time.sleep(0.003)
-                  #time.sleep(1)
-              elif i==5:
-                  GPIO.output(out1,GPIO.LOW)
-                  GPIO.output(out2,GPIO.LOW)
-                  GPIO.output(out3,GPIO.HIGH)
-                  GPIO.output(out4,GPIO.HIGH)
-                  time.sleep(0.003)
-                  #time.sleep(1)
-              elif i==6:
-                  GPIO.output(out1,GPIO.LOW)
-                  GPIO.output(out2,GPIO.LOW)
-                  GPIO.output(out3,GPIO.LOW)
-                  GPIO.output(out4,GPIO.HIGH)
-                  time.sleep(0.003)
-                  #time.sleep(1)
-              elif i==7:
-                  GPIO.output(out1,GPIO.HIGH)
-                  GPIO.output(out2,GPIO.LOW)
-                  GPIO.output(out3,GPIO.LOW)
-                  GPIO.output(out4,GPIO.HIGH)
-                  time.sleep(0.003)
-                  #time.sleep(1)
-              if i==0:
-                  i=7
-                  continue
-              i=i-1
+    if x>0 and x<=4096:
+        #x=x*-1
+        for y in range(x,0,-1):
+            if negative==1:
+                if i==7:
+                    i=0
+                else:
+                    i=i+1
+                y=y+2
+                negative=0
+            positive=1
 
+            giro_stepper(x)
 
-except KeyboardInterrupt:
-    GPIO.cleanup()
+    elif x<0 and x>=-4096:
+        x=x*-1
+        for y in range(x,0,-1):
+            if positive==1:
+                if i==0:
+                    i=7
+                else:
+                    i=i-1
+                y=y+3
+                positive=0
+            negative=1
+
+            giro_stepper(x)
