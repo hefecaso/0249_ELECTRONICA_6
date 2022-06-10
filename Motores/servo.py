04cc0736696b8c8f5cbd6b71e5d39a53c457668d
hefecaso <hfcarrerasoto@ieee.org>
2022-06-10 00:12:52 -0600

    Modificando menu

--- Motores/servo.py
+++ Motores/servo.py
@@ -39,20 +39,21 @@ def servo():
 
         elif angulo > 180 and 0 < angulo:
             print("Debe de ingresar un ángulo entre 180° y 0°")
-            selec = input("Desea regresar al menú principal? Y/N: ")
+            #selec = input("Desea regresar al menú principal? Y/N: ")
+            pass
 
-            if selec == "N":
-                print("Recuerde ingresar un ángulo entre 180° y 0°")
+            #if selec == "N":
+                #print("Recuerde ingresar un ángulo entre 180° y 0°")
 
-            else:
-                #Close GPIO & cleanup
-                #print("\nRegresando a punto de origen ángulo 0°")
-                #pwm.start(angulo_giro(0))
-                #time.sleep(2)
-                pwm.stop()
-                GPIO.cleanup()
-                print("Saliendo al menú principal")
-                break
+        else:
+            #Close GPIO & cleanup
+            #print("\nRegresando a punto de origen ángulo 0°")
+            #pwm.start(angulo_giro(0))
+            #time.sleep(2)
+            pwm.stop()
+            GPIO.cleanup()
+            print("Saliendo al menú principal")
+            break
 
 
 ''''
