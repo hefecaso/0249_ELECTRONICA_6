b18f0e4134a9b2bd4e46039af9a8fc899259ead7
hefecaso <hfcarrerasoto@ieee.org>
2022-06-16 02:29:15 -0600

    Opc 4, abre nueva terminal con programa ejecutandose, servo funciona

--- Servo/Menu.py
+++ Servo/Menu.py
@@ -1,9 +1,12 @@
-import RPi.GPIO as GPIO
+#import RPi.GPIO as GPIO
 import time
 import os
 from os import system
 #import stepper
-#import servo
+import Servo_target_virtual as stv
+
+import platform
+import subprocess
 
 
 def menu():
@@ -18,8 +21,8 @@ def menu():
     print("\n1. Elevación.")
     print("2. Azimut.")
     print("3. Azimut y elevación.")
-    #print("4. Target ISS.\n")
-    print("4. Salir.\n")
+    print("4. Target ISS.")
+    print("5. Salir.\n")
 
 
 while True:
@@ -29,47 +32,46 @@ while True:
 
     if opc == '1':
         print('====================================================================')
-        pwm.start(0)
-        exec(open("servo.py").read())
-        GPIO.cleanup()
+        #pwm.start(0)
+        #exec(open("servo.py").read())
+        #GPIO.cleanup()
         print('====================================================================')
 
     elif opc == '2':
         print('====================================================================')
-        exec(open("stepper.py").read())
-        GPIO.cleanup()
+        #exec(open("stepper.py").read())
+        #GPIO.cleanup()
         print('====================================================================')
 
     elif opc == '3':
         print('====================================================================')
-        while True:
+        #while True:
             #exec(open("servo.py").read())
-            system(f"python3 servo.py")
-            exec(open("stepper.py").read())
-            GPIO.cleanup()
-            opc2 = input("\nEjecutar otra instrucción? y/n: ")
+            #system(f"python3 servo.py")
+            ##GPIO.cleanup()
+            #opc2 = input("\nEjecutar otra instrucción? y/n: ")
 
-            if opc2 == "n":
-                break
+            #if opc2 == "n":
+                #break
         print('====================================================================')
-
-    #elif opc == '4':
-        #print('====================================================================')
-
-        #print('====================================================================')
 
     elif opc == '4':
         print('====================================================================')
+        system("gnome-terminal -- python Menu.py")
+        angulo = int(input("Ingrese valor: "))
+        stv.movimiento(angulo)
+        print('====================================================================')
+
+    elif opc == '5':
+        print('====================================================================')
         print("Saliendo del programa.")
-        pwm.stop()
-        GPIO.cleanup()
+        #pwm.stop()
+        #GPIO.cleanup()
         print('====================================================================')
         break
 
     else:
-        menu()
-        opc = input("Ingrese una opción: ")
-        os.system ("clear")
+        continue
 
 
 #Chekpoin menú, falta target ISS
