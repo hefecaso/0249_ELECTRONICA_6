0d7dfbd8d5f7f97dd42632bd74654c7546b074e1
hefecaso <hfcarrerasoto@ieee.org>
2022-06-17 14:22:14 -0600

    dasf

--- Pruebas_virtuales/Menu.py
+++ Pruebas_virtuales/Menu.py
@@ -73,7 +73,7 @@ while True:
 
     elif opc == '4':
         print('====================================================================')
-        system("lxterminal -e python isschris.py")
+        system("lxterminal -- python isschris.py")
         cuenta = contador()
         for i in range(1000):
            print(f"Azimut {next(cuenta)}° | Elevación {next(cuenta)}°")
