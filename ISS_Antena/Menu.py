3b3273f6ab0e56f2d2cc1b02bb578729305b6635
hefecaso <hfcarrerasoto@ieee.org>
2022-06-26 18:57:47 -0600

    fsdafsda

--- ISS_Antena/Menu.py
+++ ISS_Antena/Menu.py
@@ -151,7 +151,7 @@ while True:
             GPIO.output(out3,GPIO.LOW)
             GPIO.output(out4,GPIO.LOW)
             #print("ingrese un valor para rotar un angulo de 0 a 360")
-            deg = Azimut
+            deg = float(Azimut)
             x = (-1*(deg*4096)/(360))
             if x>0 and x<=4096:
                 for y in range(x,0,-1):
