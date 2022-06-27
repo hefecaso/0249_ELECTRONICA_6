6ae78b2c16198188a940b7c543b329ab9fad7c15
hefecaso <hfcarrerasoto@ieee.org>
2022-06-27 12:01:42 -0600

    Arreglando invalid literal base 10

--- ISS_Antena/Menu.py
+++ ISS_Antena/Menu.py
@@ -82,7 +82,7 @@ Azimut =  int(iss_1.az * degrees_per_radian)
 
 
 def angulo_giro(angulo):
-    giro = int(angulo)/18 +2
+    giro = (angulo)/18 +2
     return giro
 
 ####################################################
