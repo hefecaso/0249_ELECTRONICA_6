5051add248f88dd46c23141aaa68a2b1de954049
Tefy <3004287660101@ingenieria.usac.edu.gt>
2022-06-29 14:00:56 -0600

    Prueba cerco

--- ISS_Antena2/Menu.py
+++ ISS_Antena2/Menu.py
@@ -5,6 +5,10 @@ from os import system
 #import stepper
 #import servo
 
+import sys 
+sys.path.append("/isschris2")
+from isschris2 import lat, lon
+
 
 
 ####################################################
@@ -79,14 +83,15 @@ while True:
         # Colocando servo y stepper en punto de partida, dirección Norte
         print("\nRegrsando al origen: \n")
 
-        system(f"python3 servo_origin.py")
-        exec(open("stepper_origin.py").read())
-        GPIO.cleanup()
-        time.sleep(5)
+        if 6.09958 < lat < 20.143828 and -109.107194 < lon < -76.671761: 
 
+            system(f"python3 servo_origin.py")
+            exec(open("stepper_origin.py").read())
+            GPIO.cleanup()
+            time.sleep(5)
         # Moviendo dirección a la ISS
-        system(f"lxterminal -e python3 servotarget.py")
-        system(f"lxterminal -e python3 steppertarget.py")
+            system(f"lxterminal -e python3 servotarget.py")
+            system(f"lxterminal -e python3 steppertarget.py")
 
 
 
