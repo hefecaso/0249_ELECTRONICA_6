8f0b355a28f641577d2336daabea01bb1dfbba18
hefecaso <hfcarrerasoto@ieee.org>
2022-06-29 13:45:41 -0600

    Orientando al norte al salir y al iniciar menu

--- ISS_Antena2/Menu.py
+++ ISS_Antena2/Menu.py
@@ -2,12 +2,6 @@ import RPi.GPIO as GPIO
 import time
 import os
 from os import system
-import sys 
-
-
-sys.path.append("/isschris2")
-from isschris2 import lon, lat
-
 #import stepper
 #import servo
 
@@ -19,6 +13,17 @@ def menu():
     print('#    Control de la antena   #')
     print('#############################')
     exec(open("ascii.py").read())
+
+    print("\nRegrsando a elevación 0°: \n")
+
+    system(f"python3 servo_origin.py")
+
+    print("\nRegrsando a azimut 0°: \n")
+
+    exec(open("stepper_origin.py").read())
+    GPIO.cleanup()
+    time.sleep(5)
+    
     print('======================')
     print('Seleccione una opción')
     print('======================\n')
@@ -27,8 +32,7 @@ def menu():
     print("2. Azimut.")
     print("3. Azimut y elevación.")
     print("4. Target ISS.")
-    print("5. Orientar antena al norte.")
-    print("6. Salir.\n")
+    print("5. Salir.\n")
 
 
 while True:
@@ -75,15 +79,14 @@ while True:
         # Colocando servo y stepper en punto de partida, dirección Norte
         print("\nRegrsando al origen: \n")
 
-        if 6.09958 < lat < 20.143828 and -109.107194 < lon < -76.671761: 
-            system(f"python3 servo_origin.py")
-            exec(open("stepper_origin.py").read())
-            GPIO.cleanup()
-            time.sleep(5)
+        system(f"python3 servo_origin.py")
+        exec(open("stepper_origin.py").read())
+        GPIO.cleanup()
+        time.sleep(5)
 
-            # Moviendo dirección a la ISS
-            system(f"lxterminal -e python3 servotarget.py")
-            system(f"lxterminal -e python3 steppertarget.py")
+        # Moviendo dirección a la ISS
+        system(f"lxterminal -e python3 servotarget.py")
+        system(f"lxterminal -e python3 steppertarget.py")
 
 
 
@@ -102,10 +105,6 @@ while True:
         GPIO.cleanup()
         time.sleep(5)
 
-        print('====================================================================')
-
-    elif opc == '6':
-        print('====================================================================')
         print("Saliendo del programa.")
         pwm.stop()
         GPIO.cleanup()
