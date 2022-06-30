2d246fdb08de1d755245de617dff5f0ca10219f8
Tefy <3004287660101@ingenieria.usac.edu.gt>
2022-06-30 00:46:30 -0600

    Prueba cuadrado

--- ISS_Antena2/isschris2.py
+++ ISS_Antena2/isschris2.py
@@ -65,10 +65,9 @@ def tracker():
             gt.dot(gt.goto(float(longitud),float(latitud)))
             cerco.pencolor("magenta")
             cerco.dot(cerco.goto(float(-107.324236),float(19.819178)))
-            (cerco.goto(float(-107.324236),float(19.819178)))
-            (cerco.goto(float(-76.671761),float(20.143828)))
-            (cerco.goto(float(-76.671761),float(10.808493)))
-            (cerco.goto(float(-109.107194),float(6.09958)))
+            cerco.dot(cerco.goto(float(-76.671761),float(20.143828)))
+            cerco.dot(cerco.goto(float(-76.671761),float(10.808493)))
+            cerco.dot(cerco.goto(float(-109.107194),float(6.09958)))
             degrees_per_radian = 180.0 / math.pi
             home = ephem.Observer()
             home.lon = '-90.51327'
